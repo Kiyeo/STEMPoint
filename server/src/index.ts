cdde71d2025adbeda226f49fe30ba971b9d1650b
kiyeo <leojkeo@outlook.com>
2020-09-17 23:08:12 +1200

    Add login page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import {MikroORM} from "@mikro-orm/core";
 import {__prod__} from "./constants";
-//import {Post} from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
@@ -12,7 +11,7 @@ import {UserResolver} from './resolvers/user';
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
-import {MyContext} from './types';
+import cors from 'cors'
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -27,6 +26,10 @@ const main = async () => {
   const RedisStore = connectRedis(session)
   const redisClient = redis.createClient()
 
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }))
   app.use(
     session({
       name: 'qid',
@@ -53,7 +56,10 @@ const main = async () => {
     context: ({req, res}) => ({em: orm.em, req, res})
   });
 
-  apolloServer.applyMiddleware({app});
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000')
--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -3,26 +3,23 @@ import {Form, Formik} from 'formik'
 import {Box, Button} from '@chakra-ui/core'
 import {Wrapper} from '../components/Wrapper'
 import {InputField} from '../components/inputField'
-import {useRegisterMutation} from '../generated/graphql'
+import {useLoginMutation} from '../generated/graphql'
 import {toErrorMap} from '../utils/toErrorMap'
 import { useRouter} from 'next/router'
 
-interface registerProps {
-}
 
 
-const Register: React.FC<registerProps> = ({}) => {
+const Login: React.FC<{}> = ({}) => {
     const router = useRouter();
-    const [,register] = useRegisterMutation();
+    const [,login] = useLoginMutation();
     return (
       <Wrapper variant='small'>
       <Formik initialValues={{username: '', password: ''}}
          onSubmit={async (values, {setErrors}) => {
-            console.log(values)
-            const response = await register(values);
-            if(response.data?.register.errors){
-                setErrors(toErrorMap(response.data.register.errors)); 
-            } else if (response.data?.register.user) {
+           const response = await login({options: values});
+            if(response.data?.login.errors){
+                setErrors(toErrorMap(response.data.login.errors)); 
+            } else if (response.data?.login.user) {
                 // worked
                 router.push("/")
             }
@@ -34,7 +31,7 @@ const Register: React.FC<registerProps> = ({}) => {
             <Box mt={4}>
             <InputField name="password" placeholder="password" label="Password" type="password"/>
             </Box>
-            <Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal">register</Button>
+            <Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal">Login</Button>
           </Form>
         )}
       </Formik>
@@ -42,4 +39,4 @@ const Register: React.FC<registerProps> = ({}) => {
     )
 }
 
-export default Register
+export default Login
