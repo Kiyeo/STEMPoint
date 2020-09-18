dc2b431df7884c5841f7670dfee7265487db6cf2
kiyeo <leojkeo@outlook.com>
2020-09-18 22:12:16 +1200

    Add SSR on Next.js using URQL

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -137,6 +137,17 @@ export type MeQuery = (
   )> }
 );
 
+export type PostsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type PostsQuery = (
+  { __typename?: 'Query' }
+  & { posts: Array<(
+    { __typename?: 'Post' }
+    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title'>
+  )> }
+);
+
 export type RegisterMutationVariables = Exact<{
   username: Scalars['String'];
   password: Scalars['String'];
@@ -199,6 +210,20 @@ export const MeDocument = gql`
 
 export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {}) {
   return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
+};
+export const PostsDocument = gql`
+    query Posts {
+  posts {
+    id
+    createdAt
+    updatedAt
+    title
+  }
+}
+    `;
+
+export function usePostsQuery(options: Omit<Urql.UseQueryArgs<PostsQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<PostsQuery>({ query: PostsDocument, ...options });
 };
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -6,6 +6,8 @@ import {InputField} from '../components/inputField'
 import {useRegisterMutation} from '../generated/graphql'
 import {toErrorMap} from '../utils/toErrorMap'
 import { useRouter} from 'next/router'
+import {createUrqlClient} from '../utils/createUrqlClient'
+import {withUrqlClient} from 'next-urql'
 
 interface registerProps {
 }
@@ -41,4 +43,4 @@ const Register: React.FC<registerProps> = ({}) => {
     )
 }
 
-export default Register
+export default withUrqlClient(createUrqlClient)(Register)
